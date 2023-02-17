b2924951b5b79bf9d843631c9d3d204f16b5808c
Marina Glancy <marina.glancy@gmail.com>
2023-02-17 09:57:31 +0000

    Shortcuts, continue command

--- README.md
+++ README.md
@@ -1,7 +1,32 @@
-# VS Code extension to open all files in a folder
+# VS Code extension "Open all files for Diagnostics"
 
-## Features
+Opens all files in a folder and all subfolders recursively.
 
-Opens all files in a folder and all subfolders recursively. This allows
-to have an overview of all the problems in the files since VS Code does not
-show problems in the files that have not been opened.
+## Using Open all files extension to review diagnostics (open large number of files in batches)
+
+This extension is essential for reviewing problems on big projects. VS Code
+only shows diagnostics for the opened files and automatically unloads files
+and removes them from Problems view when too many files are open.
+
+- Open "Problems" view (Ctrl+Shift+M)
+- Right click on a folder in Explorer and choose "Open all files", this will open first 30 files
+- Review the detected problems, make changes as needed
+- Close all open files (Ctrl+K W)
+- Right click on the folder again and choose "Open all files (continue)", this will open the next 30 files
+- Rinse and repeat
+
+When using extension for diagnostics, you can adjust some settings, for example, increase delay between
+opening new files and choose not to open them in editor.
+
+![Open All Files example](https://github.com/marinaglancy/vscode-open-all-files/blob/master/media/open-all-files.png)
+
+### Extension settings
+
+| Setting                            | Description                                                                                                                                                     | Default                                   |
+|------------------------------------|-----------------------------------------------------------------------------------------------------------------------------------------------------------------|-------------------------------------------|
+| Limit                              | Maximum number of files to open in one operation. If the folder has more files, the next command execution will start from where it finished last time.         | 30                                        |
+| Open Files                         | Files to open. Examples: ```**``` - all files, ```**/*.{php,js}``` - only PHP and JS files",                                                                    | **                                        |
+| Exclude Files                      | Configure glob patterns to exclude certain files and folders from opening. Relative paths are calculated from the workspace root (not the folder being opened). | ```**/.git/**, **/node_modules/**, ...``` |
+| Auto Exclude Framework Suggestions | Automatically detect other files to exclude for some common projects or frameworks                                                                              | true                                      |
+| Delay                              | Delay (in ms) between opening files to allow diagnostics to catch up with the newly loaded files                                                                | 200                                       |
+| Open For Diagnostics Only          | Open files for diagnostics only, do not show in editor. The 'Problems' view will display all problems found in these files.                                     | false                                     |
--- src/extension.ts
+++ src/extension.ts
@@ -1,46 +1,89 @@
-import { ExtensionContext, Uri, commands, window, workspace } from 'vscode';
+import { ExtensionContext, StatusBarAlignment, StatusBarItem, Uri, commands, window, workspace } from 'vscode';
 import { readDirectoryRecursively } from './fileutil';
+
+let myStatusBarItem: StatusBarItem;
+let lastFolder:Uri|null = null;
+let lastFolderIndex:number = 0;
 
 export function activate(context: ExtensionContext) {
 
+  const commandId = 'vscode-open-all-files.openAllFiles';
+  const commandId2 = 'vscode-open-all-files.openAllFilesContinue';
+  setLastFolder(null);
   context.subscriptions.push(
-    commands.registerCommand('vscode-open-all-files.openAllFiles', (e) => openAllFiles(e)));
+    commands.registerCommand(commandId, (uri) => openAllFiles(uri)));
+  context.subscriptions.push(
+    commands.registerCommand(commandId2, () => openAllFilesContinue()));
+
+  myStatusBarItem = window.createStatusBarItem(StatusBarAlignment.Right, 100);
+  context.subscriptions.push(myStatusBarItem);
 }
 
 export function deactivate() {}
 
-let lastFolder:string|null = null;
-let lastFolderIndex:number = 0;
-
-async function openAllFiles(uri:Uri) {
-  const maxFiles = (workspace.getConfiguration().get<number>('openAllFiles.maxFiles') ?? 30);
-  let files:Array<Uri> = await readDirectoryRecursively(uri.fsPath),
-      firstIndex = 0, lastIndex = Math.min(maxFiles, files.length);
-  if (files.length > maxFiles) {
-    if (lastFolder === uri.fsPath && lastFolderIndex < files.length) {
-      firstIndex = lastFolderIndex;
-      lastIndex = Math.min(firstIndex + maxFiles, files.length);
-    }
-    window.showInformationMessage(`Opening ${firstIndex + 1}-${lastIndex} of ${files.length} files`);
-    lastFolder = uri.fsPath;
-    lastFolderIndex = lastIndex;
+function setLastFolder(uri:Uri|null, idx:number = 0) {
+  if (uri) {
+    lastFolder = uri;
+    commands.executeCommand('setContext', 'openAllFiles.lastFolder', [lastFolder.fsPath]);
+    commands.executeCommand('setContext', 'openAllFiles.hasLastFolder', true);
+    lastFolderIndex = idx;
   } else {
     lastFolder = null;
+    commands.executeCommand('setContext', 'openAllFiles.lastFolder', []);
+    commands.executeCommand('setContext', 'openAllFiles.hasLastFolder', false);
+    lastFolderIndex = 0;
   }
+}
+
+async function openAllFilesContinue() {
+  if (lastFolder) {
+    await openAllFiles(lastFolder, true);
+  } else {
+    window.showErrorMessage(`No more files to open`);
+  }
+}
+
+async function openAllFiles(uri:Uri, continueLastFolder:boolean=false) {
+  myStatusBarItem.text = `Retrieving all folder files...`;
+  myStatusBarItem.show();
+
+  const maxFiles = (workspace.getConfiguration().get<number>('openAllFiles.limit') ?? 30);
+  let files:Array<Uri> = await readDirectoryRecursively(uri.fsPath),
+      firstIndex = 0, lastIndex = Math.min(maxFiles, files.length);
+
+  if (continueLastFolder) {
+    if (lastFolderIndex < files.length) {
+      firstIndex = lastFolderIndex;
+      lastIndex = Math.min(firstIndex + maxFiles, files.length);
+    } else {
+      window.showErrorMessage(`No more files to open`);
+      setLastFolder(null);
+      myStatusBarItem.hide();
+      return;
+    }
+  }
+  if (files.length > maxFiles || firstIndex > 0) {
+    window.showInformationMessage(`Opening files ${firstIndex + 1}-${lastIndex} out of ${files.length}`);
+  }
+  setLastFolder(files.length > lastIndex ? uri : null, lastIndex);
+
   const diagnOnly:boolean = workspace.getConfiguration().get<boolean>('openAllFiles.openForDiagnosticsOnly') ?? false;
+  const delay:number = workspace.getConfiguration().get<number>('openAllFiles.delay') ?? 200;
   for (let i = firstIndex; i < lastIndex; i++) {
     try {
-
       if (diagnOnly) {
         await workspace.openTextDocument(files[i]);
       } else {
         await commands.executeCommand('vscode.open', files[i]);
       }
-      await sleep(200);
+      myStatusBarItem.text = `Opened ${i-firstIndex+1}/${lastIndex-firstIndex} files`;
+      delay > 0 && await sleep(delay);
     } catch (e) {
       console.error(e);
     };
   }
+
+  myStatusBarItem.hide();
 }
 
 function sleep(ms:number) {
